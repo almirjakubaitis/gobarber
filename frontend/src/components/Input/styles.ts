cf7737045a694c24b9c7f62e2a4662fda2495e2c
Almir Jakubaitis <almir@icone3.com.br>
2020-07-23 16:02:49 -0300

    Usabilidade do Input

--- frontend/src/components/Input/styles.ts
+++ frontend/src/components/Input/styles.ts
@@ -1,11 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
-  padding: 16px;
+  padding: 2px 16px;
   width: 100%;
+
+  border: 2px solid #232129;
   color: #666360;
 
   display: flex;
@@ -15,11 +21,26 @@ export const Container = styled.div`
     margin-top: 8px;
   }
 
+  ${props =>
+    props.isFocused &&
+    css`
+      border: 2px solid #ff9000;
+      color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
   input {
     flex: 1;
     border: 0;
     background: transparent;
     color: #f4ede8;
+    height: 50px;
+    line-height: 50px;
 
     &::placeholder {
       color: #666360;
