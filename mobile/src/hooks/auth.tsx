61287b2d045bf58a452a3474dc0ff8a43abbf08c
Almir Jakubaitis <almir@icone3.com.br>
2020-11-05 12:09:05 -0300

    Mobile: Rotas Privadas

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -20,6 +20,7 @@ interface SingInCredentials {
 
 interface AuthContextData {
   user: Record<string, unknown>;
+  loading: boolean;
   signIn(credentials: SingInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -33,6 +34,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({children}) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
@@ -47,6 +49,8 @@ const AuthProvider: React.FC = ({children}) => {
       if (token[1] && user[1]) {
         setData({token: token[1], user: JSON.parse(user[1])});
       }
+
+      setLoading(false);
     }
 
     loadStorageData();
@@ -84,7 +88,7 @@ const AuthProvider: React.FC = ({children}) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{user: data.user, signIn, signOut}}>
+    <AuthContext.Provider value={{user: data.user, loading, signIn, signOut}}>
       {children}
     </AuthContext.Provider>
   );
