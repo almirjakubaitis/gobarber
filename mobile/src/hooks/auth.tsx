94e0a53aa87044e87675ec77d4870309bd2ef5b5
Almir Jakubaitis <almir@icone3.com.br>
2020-11-04 22:06:45 -0300

    Mobile: Autenticação de Usuários

---
+++ mobile/src/hooks/auth.tsx
@@ -0,0 +1,103 @@
+import React, {
+  createContext,
+  useCallback,
+  useState,
+  useContext,
+  useEffect,
+} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: Record<string, unknown>; // object
+}
+
+interface SingInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: Record<string, unknown>;
+  signIn(credentials: SingInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+// variável inicialmente com valor vazio -> forçando {} as AuthContext
+// para remover o erro do Typescript
+
+// Outra forma de fazer, mas que poderia dar erros a frente
+// const authContext = createContext<AuthContext | null>(null);
+
+const AuthProvider: React.FC = ({children}) => {
+  const [data, setData] = useState<AuthState>({} as AuthState);
+
+  useEffect(() => {
+    async function loadStorageData(): Promise<void> {
+      // const token = await AsyncStorage.getItem('@Gobarber:token');
+      // const user = await AsyncStorage.getItem('@Gobarber:user');
+
+      const [token, user] = await AsyncStorage.multiGet([
+        '@Gobarber:token',
+        '@Gobarber:user',
+      ]);
+
+      if (token[1] && user[1]) {
+        setData({token: token[1], user: JSON.parse(user[1])});
+      }
+    }
+
+    loadStorageData();
+  }, []);
+
+  const signIn = useCallback(async ({email, password}) => {
+    // console.log('signIn');
+
+    // const response = await api.post<{ token: string; user: string }>
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    // console.log(response.data);
+    const {token, user} = response.data;
+
+    // await AsyncStorage.setItem('@Gobarber:token', token);
+    // await AsyncStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+    await AsyncStorage.multiSet([
+      ['@Gobarber:token', token],
+      ['@Gobarber:user', JSON.stringify(user)],
+    ]);
+
+    setData({token, user});
+  }, []);
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(['@Gobarber:token', '@Gobarber:user']);
+    // await AsyncStorage.removeItem('@Gobarber:token');
+    // await AsyncStorage.removeItem('@Gobarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{user: data.user, signIn, signOut}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+
+  return context;
+}
+
+export {AuthProvider, useAuth};
--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -15,6 +15,8 @@ import * as Yup from 'yup';
 import {Form} from '@unform/mobile';
 import {FormHandles} from '@unform/core';
 
+import {useAuth} from '../../hooks/auth';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -40,53 +42,61 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
 
+  // const {signIn, user} = useAuth();
+  // console.log(user);
+
+  const {signIn} = useAuth();
+
   const passwordInputRef = useRef<TextInput>(null);
 
   // const handleSignIn = useCallback((data: object) => {
   //   console.log(data);
   // }, []);
 
-  const handleSignIn = useCallback(async (data: SignInFormData) => {
-    // console.log(data);
-    try {
-      formRef.current?.setErrors({});
+  const handleSignIn = useCallback(
+    async (data: SignInFormData) => {
+      // console.log(data);
+      try {
+        formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um e-mail válido'),
-        password: Yup.string().required('Senha obrigatória'),
-      });
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-      // await signIn({
-      //   email: data.email,
-      //   password: data.password,
-      // });
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
 
-      // history.push('/dashboard');
-    } catch (err) {
-      // console.log(err);
+        // history.push('/dashboard');
+      } catch (err) {
+        // console.log(err);
 
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
 
-        formRef.current?.setErrors(errors);
+          formRef.current?.setErrors(errors);
 
-        return;
+          return;
+        }
+
+        // disparar um toast / Alert
+
+        Alert.alert(
+          'Erro na autenticação',
+          'Ocorreu um erro ao fazer login, verifique as credenciais',
+        );
       }
-
-      // disparar um toast / Alert
-
-      Alert.alert(
-        'Erro na autenticação',
-        'Ocorreu um erro ao fazer login, verifique as credenciais',
-      );
-    }
-  }, []);
+    },
+    [signIn],
+  );
 
   return (
     <>
