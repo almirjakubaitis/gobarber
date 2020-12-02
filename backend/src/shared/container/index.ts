6e10cf0136dee43ac70cc5480003ec8e381b6ef7
Almir Jakubaitis <almir@icone3.com.br>
2020-12-01 21:23:44 -0300

    Backend_nivel_4: Inejção de dependências

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -3,7 +3,16 @@ import { container } from 'tsyringe';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+// registerSingleton Istancia a classe uma única vez no ciclo da aplicação
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
   AppointmentsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
 );
