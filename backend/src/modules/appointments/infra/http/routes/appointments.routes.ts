5998b3ffdc2c48fe3929e4eddd4395739e43a9d8
Almir Jakubaitis <almir@icone3.com.br>
2021-01-25 18:36:48 -0300

    Nivel 4: Finalizando Backend: validando dados okay

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
@@ -21,7 +22,19 @@ appointmentsRouter.use(ensureAuthenticated);
 // Antes do controler escrevíamos assim
 // appointmentsRouter.post('/', async (request, response) => {});
 
-appointmentsRouter.post('/', appointmentsController.create);
+// appointmentsRouter.post('/', appointmentsController.create);
+// appointmentsRouter.get('/me', providerAppointmentsController.index);
+//
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.string().required(),
+    },
+  }),
+  appointmentsController.create,
+);
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
