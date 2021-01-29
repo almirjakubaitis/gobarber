484ce71767e3ce2634a9a62043896c3f52f76350
Almir Jakubaitis <almir@icone3.com.br>
2021-01-29 17:31:22 -0300

    Nivel 5: Ajustes na API: serialização no cache (classToClass) em appointments - controller e cache

--- backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
@@ -45,7 +46,7 @@ export default class ListProviderAppopintmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
 
       // console.log('buscou do banco');
     }
