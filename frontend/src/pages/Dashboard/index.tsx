c3fbe9786f6940816d9b17295feba19016c37267
Almir Jakubaitis <almir@icone3.com.br>
2021-02-01 14:54:54 -0300

    Nivel 5: Frontend: Criando Dashobar: Criando Proximo agendamento

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -27,6 +36,32 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://xesque.rocketseat.dev/users/avatar/profile-feda561b-f903-42ac-8bcb-70a4a9a5cb7d.jpg"
+                alt="Almir Jakubaitis"
+              />
+              <strong>Almir Jakubaitis</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar>bbb</Calendar>
+      </Content>
     </Container>
   );
 };
