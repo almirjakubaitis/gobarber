32f33598226d442000b319355a9254ecd8fdb5f5
Almir Jakubaitis <almir@icone3.com.br>
2021-02-04 20:05:34 -0300

    Nivel 5: Mobile: Agendamento - Estrutura da criação

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import React, {useCallback} from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import {useAuth} from '../../hooks/auth';
 
-import {View} from 'react-native';
+import {Container, Header, BackButton, HeaderTitle, UserAvatar} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+  // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} />
+  const route = useRoute();
+  const {providerId} = route.params as RouteParams;
+
+  const {goBack} = useNavigation();
+
+  const {user} = useAuth();
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  // console.log(routeParams.providerId);
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} />
+    <Container>
+      <Header>
+        <BackButton
+          onPress={() => {
+            navigateBack();
+          }}>
+          <Icon name="chevron-left" size={24} color="#959991" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{uri: user.avatar_url}} />
+      </Header>
+    </Container>
   );
 };
 
