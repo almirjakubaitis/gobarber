1ed9dd460eaafd799d085caeff96ba94ecfb89f5
Almir Jakubaitis <almir@icone3.com.br>
2021-02-04 17:25:33 -0300

    Nivel 5: Mobile: Dashboard - Buscando Providers da API

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,8 +1,10 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {View} from 'react-native';
 
 import {useAuth} from '../../hooks/auth';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -11,15 +13,31 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
 } from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+
   const {signOut, user} = useAuth();
   const {navigate} = useNavigation();
 
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, [setProviders]);
+
   const navigateToProfile = useCallback(() => {
+    signOut();
     navigate('Profile');
-  }, [navigate]);
+  }, [navigate, signOut]);
 
   return (
     <Container>
@@ -33,6 +51,12 @@ const Dashboard: React.FC = () => {
           <UserAvatar source={{uri: user.avatar_url}} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({item}) => <UserName>{item.name}</UserName>}
+      />
     </Container>
   );
 };
