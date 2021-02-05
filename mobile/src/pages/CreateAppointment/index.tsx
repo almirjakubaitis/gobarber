0f7971da2a0cacfb81325374bbc7f7e7cdb246b0
Almir Jakubaitis <almir@icone3.com.br>
2021-02-05 15:51:54 -0300

    Nivel 5: Mobile: Agendamento - Disponibilidade por período

--- mobile/src/pages/CreateAppointment/index.tsx
+++ mobile/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,10 @@
 import {useNavigation, useRoute} from '@react-navigation/native';
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import {Platform} from 'react-native';
+
+import {format} from 'date-fns';
 
 import {useAuth} from '../../hooks/auth';
 import api from '../../services/api';
@@ -102,6 +104,30 @@ const CreateAppointment: React.FC = () => {
     [],
   );
 
+  const morningAvailability = useMemo(() => {
+    return availability
+      .filter(({hour}) => hour < 12)
+      .map(({hour, available}) => {
+        return {
+          hour,
+          available,
+          hourFormatted: format(new Date().setHours(hour), 'HH:00'),
+        };
+      });
+  }, [availability]);
+
+  const afternoonAvailability = useMemo(() => {
+    return availability
+      .filter(({hour}) => hour >= 12)
+      .map(({hour, available}) => {
+        return {
+          hour,
+          available,
+          hourFormatted: format(new Date().setHours(hour), 'HH:00'),
+        };
+      });
+  }, [availability]);
+
   // console.log(routeParams.providerId);
 
   return (
@@ -159,6 +185,14 @@ const CreateAppointment: React.FC = () => {
           />
         )}
       </Calendar>
+
+      {morningAvailability.map(({hourFormatted, available}) => (
+        <Title key={hourFormatted}>{hourFormatted}</Title>
+      ))}
+
+      {afternoonAvailability.map(({hourFormatted, available}) => (
+        <Title key={hourFormatted}>{hourFormatted}</Title>
+      ))}
     </Container>
   );
 };
