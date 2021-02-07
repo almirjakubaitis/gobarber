be474756391acb52556287498df8ad0e6ac4c8aa
Almir Jakubaitis <almir@icone3.com.br>
2021-02-06 22:07:34 -0300

    Nivel 5: Mobile: sucesso - Formatando a data

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,11 +1,22 @@
-import React, {useCallback} from 'react';
-import {useNavigation} from '@react-navigation/native';
+import React, {useCallback, useMemo} from 'react';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import {Container, Title, Description, OkButton, OkButtonText} from './styles';
+import {format} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
   const {reset} = useNavigation();
+
+  const {params} = useRoute();
+
+  const routeParams = params as RouteParams;
+
   const handleOKPressed = useCallback(() => {
     reset({
       routes: [
@@ -17,15 +28,23 @@ const AppointmentCreated: React.FC = () => {
     });
   }, [reset]);
 
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia ' dd 'de ' MMMM 'de ' yyyy 'às ' HH:mm'h'",
+      {
+        locale: ptBR,
+      },
+    );
+  }, [routeParams.date]);
+
   return (
     <>
       <Container>
         <Icon name="check" size={80} color="#04d361" />
 
         <Title>Agendamento concluído</Title>
-        <Description>
-          Terça, dia 14 de março de 2020 às 12:00h com Pedro Petovisk;
-        </Description>
+        <Description>{formattedDate}.</Description>
 
         <OkButton onPress={handleOKPressed}>
           <OkButtonText>Ok</OkButtonText>
